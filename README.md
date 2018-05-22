1761dba2f434da7206f7e9a73763c5d2321a2a28
xuesong <xuesong@uoko.com>
2018-05-22 13:37:46 +0800

    fix position issue

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 A simple input placeholder polyfill,with vanilla js.Tested in IE 8/9/10.
 
-##How does it work?
+## How does it work?
 
-This polyfill simply appends a &lt;span class="\_fake_placeholder\_"&gt; to every input element which has placeholder attribute,using focus and blur event to show or hide this fake placeholder.
-<b>warning:you should normalize or reset the default styles so that the fake placeholder can appeared in right place.</b>
+This polyfill append a &lt;span class="\_fake_placeholder\_"&gt; to every input element which has _placeholder_ attribute as placeholder,using focus and blur event to show and hide this fake placeholder.
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-;(function fixHolder(win,doc,undefined){
-    var isSupport = 'placeholder' in doc.createElement('input');
+; (function fixPlaceholder(win, doc, undefined) {
+    var support = 'placeholder' in doc.createElement('input');
 
-    if(isSupport){
-        return ;
+    if (support) {
+        return;
     }
-    function byAttr(el,attr) {
-        if(!el){
-            return ;
+    function byAttr(el, attr) {
+        if (!el) {
+            return;
         }
         var ret = [];
         el = el.length ? el : [el];
 
-        for(var i=0;i<el.length;i++){
+        for (var i = 0; i < el.length; i++) {
 
-            if(el[i].getAttribute && el[i].getAttribute(attr) !== null){
+            if (el[i].getAttribute && el[i].getAttribute(attr) !== null) {
                 ret.push(el[i]);
             }
         }
@@ -24,67 +24,66 @@
 
         return String.prototype.slice.call(obj) === '[object Object]';
     }
-    function pageX(el){
-
+    function pageX(el) {
         return el.offsetParent ? el.offsetLeft + pageX(el.offsetParent) : el.offsetLeft;
     }
-    function pageY(el){
-
-        return el.offsetParent ? el.offsetTop + pageX(el.offsetParent) : el.offsetTop;
+    function pageY(el) {
+        return el.offsetParent ? el.offsetTop + pageY(el.offsetParent) : el.offsetTop;
     }
-    function parentX(el){
 
-        return el.offsetParent === el.parentNode ? el.offsetLeft : pageX(el) - pageX(el.parentNode);
-    }
-    function parentY(el){
+    function getStyle(el, style) {
 
-        return el.offsetParent === el.parentNode ? el.offsetTop : pageY(el) - pageY(el.parentNode);
-    }
-    function getStyle(el,style){
-
-        if(!style){
+        if (!style) {
 
             return null;
         }
 
-        if(el.currentStyle){
+        if (el.currentStyle) {
 
             return el.currentStyle[style];
         }
 
-        if(win.getComputedStyle){
-            style = style.replace(/([A-Z])/,'-$1').toLocaleLowerCase();
+        if (win.getComputedStyle) {
+            style = style.replace(/([A-Z])/, '-$1').toLocaleLowerCase();
 
-            return win.getComputedStyle(el,'')[style];
+            return win.getComputedStyle(el, '')[style];
         }
 
         return null;
     }
-    function trim(val){
-        val && val.replace(/^(\s+)|(\s+)$/,'');
+    function trim(val) {
+        val && val.replace(/^(\s+)|(\s+)$/, '');
 
         return val;
     }
-    function createNode(name,prop){
+
+    function bind(fn, self) {
+        return function () {
+            var args = [].slice(arguments);
+            fn.call(self, args);
+        }
+    }
+
+    function createNode(name, prop) {
         var node = doc.createElement(name);
 
-        if(!prop){
+        if (!prop) {
 
             return node;
         }
 
-        if(!isObj(prop)){
+        if (!isObj(prop)) {
 
             throw new Error('argument should be a Object');
         }
 
-        for(var i in prop){
+        for (var i in prop) {
 
-            if(prop.hasOwnProperty(i)){
+            if (prop.hasOwnProperty(i)) {
 
-                if(isObj(prop[i])){
-                    for(var j in prop[i]){
-                        if(prop[i].hasOwnProperty(j)){
+                if (isObj(prop[i])) {
+                    for (var j in prop[i]) {
+                        if (prop[i].hasOwnProperty(j)) {
                             node.style[j] = prop[i][j];
                         }
                     }
@@ -97,79 +96,105 @@
 
         return node;
     }
-    function addEvent(el,evt,cb){
 
-        if(el.addEventListener){
-            el.addEventListener(evt,cb,false);
+    function addEvent(el, evt, hanlder) {
 
-        }else{
-            el.attachEvent('on'+evt,function(e){
+        if (el.addEventListener) {
+            el.addEventListener(evt, hanlder, false);
+
+        } else {
+            el.attachEvent('on' + evt, function (e) {
                 e = e || win.event;
                 e.target = e.srcElement;
-                e.stopPropagation = function(){
+                e.stopPropagation = function () {
                     e.cancelBubble = true;
                 };
-                e.preventDefault = function(){
+                e.preventDefault = function () {
                     e.returnValue = false;
                 };
-                cb.call(el,e);
+                hanlder.call(el, e);
             });
         }
     }
 
+    function removeEvent(el, evt, hanlder) {
+        if (el.addEventListener) {
+            el.removeEventListener(evt, hanlder, false);
 
-    function fix(els){
-        if(/Array/.test(Object.prototype.toString.call(els)) && !els.length){
-            return ;
+        } else {
+            el.detachEvent('on' + evt, hanlder);
+        }
+    }
+
+
+    function fix(els) {
+        if (/Array/.test(Object.prototype.toString.call(els)) && !els.length) {
+            return;
         }
         els = els.length ? els : [els];
 
-        for(var i=0;i<els.length;i++){
-            var parent = els[i].parentNode || doc.body;
-            var pos = getStyle(parent,'position');
-            var top = parseInt(getStyle(els[i],'paddingTop'),10) + parentY(els[i]);
-            var left = parseInt(getStyle(els[i],'paddingLeft'),10) + parentX(els[i]);
-            var t =  els[i].getAttribute('placeholder');
+        for (var i = 0; i < els.length; i++) {
+            var el = els[i];
+            var parent = el.parentNode;
+            var parentPos = getStyle(el, 'position');
+            var top = pageY(el) - pageY(parent);
+            var left = parseInt(getStyle(el, 'paddingLeft'), 10) + pageX(el) - pageX(parent);
+            var text = el.getAttribute('placeholder');
 
-            if(pos.toLowerCase() === 'static'){
+            var placeholder = createNode('span', {
+                style: {
+                    position: 'absolute',
+                    left: left + 'px',
+                    top: top + 'px',
+                    whiteSpace: 'nowrap',
+                    pointerEvents: 'none',
+                    height: el.offsetHeight + 'px',
+                    lineHeight: el.offsetHeight + 'px'
+                },
+                className: '_fake_placeholder_',
+                innerHTML: text
+            });
+
+            if (parentPos === 'static') {
                 parent.style.position = 'relative';
             }
 
-            var node = createNode('span',{
-                style:{
-                    position:'absolute',
-                    left:left + 'px',
-                    top:top + 'px',
-                    whiteSpace:'nowrap',
-                    lineHeight:getStyle(els[i],'lineHeight')
-                },
-                className:'_fake_placeholder_',
-                innerHTML:t
-            });
+            el.placeholder = placeholder;
+            placeholder.el = el;
+            parent.appendChild(placeholder);
+            el.value !== '' && (placeholder.style.display = 'none');
 
-            els[i].fakeHolder = node;
-            node.el = els[i];
-            parent.appendChild(node);
-            els[i].value !== '' && (node.style.display = 'none');
-            addEvent(els[i].fakeHolder,'click',function(e){
+            var onPlaceholderClick = bind(function (e) {
                 this.el.focus();
-            });
-            addEvent(els[i],'focus',function(e){
-                this.fakeHolder.style.display = 'none';
-            });
-            addEvent(els[i],'blur',function(e){
-                var val = trim(this.value);
+            }, placeholder);
 
-                if(val === ''){
-                    this.fakeHolder.style.display = 'block';
+            var onELeFocus = bind(function () {
+                this.placeholder.style.display = 'none';
+            }, el)
+
+            var onEleBlur = bind(function () {
+                var val = trim(this.value);
+                if (val === '') {
+                    this.placeholder.style.display = 'block';
                 }
-            })
+            }, el);
+
+            addEvent(el.placeholder, 'click', onPlaceholderClick);
+            addEvent(el, 'focus', onELeFocus);
+            addEvent(el, 'blur', onEleBlur);
+
+            el.$disdroyPlaceHolder = function () {
+                removeEvent(this.placeholder, 'click', onPlaceholderClick);
+                removeEvent(this, 'focus', onELeFocus);
+                removeEvent(this, 'blur', onEleBlur);
+                parent.removeChild(this.placeholder);
+                this.placeholder = null;
+            }
         }
     }
 
     var els = doc.getElementsByTagName('input');
-    els = byAttr(els,'placeholder');
-
+    els = byAttr(els, 'placeholder');
     fix(els);
 
-})(window,document);
+})(window, document)
